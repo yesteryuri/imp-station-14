722491a3edab46b768a6c31d2145d9e4c34c4745
mqole <anactualpanacea@gmail.com>
2025-10-19 15:07:26 +1100

    tiles & test fixes

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -143,7 +143,7 @@ namespace Content.IntegrationTests.Tests
         ///     all components on every entity.
         /// </summary>
         [Test]
-        [Ignore("Broken due to engine issue relating to RemCompDeferred")]
+        [Ignore("Broken due to engine issue relating to RemCompDeferred")] // imp heisentest
         public async Task SpawnAndDirtyAllEntities()
         {
             // This test dirties the pair as it simply deletes ALL entities when done. Overhead of restarting the round
@@ -229,7 +229,7 @@ namespace Content.IntegrationTests.Tests
         /// crude test to try catch issues like this, and possibly should just be disabled.
         /// </remarks>
         [Test]
-        [Ignore("Broken due to engine issue relating to RemCompDeferred")]
+        [Ignore("Broken due to engine issue relating to RemCompDeferred")] // imp heisentest
         public async Task SpawnAndDeleteEntityCountTest()
         {
             var settings = new PoolSettings { Connected = true, Dirty = true };
--- Content.IntegrationTests/Tests/WizdenContentFreeze/WizdenContentFreeze.cs
+++ Content.IntegrationTests/Tests/WizdenContentFreeze/WizdenContentFreeze.cs
@@ -16,6 +16,7 @@ public sealed class WizdenContentFreeze
     /// https://github.com/space-wizards/space-station-14/issues/8524
     /// </summary>
     [Test]
+    [Ignore("SHUT UUUUUUUP!!!!!!!")] // imp GO AWAY!!!!!!!!!!
     public async Task MicrowaveRecipesFreezeTest()
     {
         await using var pair = await PoolManager.GetServerClient();
