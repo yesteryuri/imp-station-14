296a36d405c7da8357451db5e41eeb16cf0c2cd0
mqole <anactualpanacea@gmail.com>
2025-10-19 18:00:19 +1100

    mech & circuit fixes

--- Content.Server/Construction/ConstructionSystem.Interactions.cs
+++ Content.Server/Construction/ConstructionSystem.Interactions.cs
@@ -423,7 +423,7 @@ namespace Content.Server.Construction
                         break;
 
                     if (partAssemblyStep.Condition(uid, EntityManager))
-                        return HandleResult.True;
+                        return validation ? HandleResult.Validated : HandleResult.True;  // imp: fixing an upstream bug, was HandleResult.True
                     return HandleResult.False;
                 }
 
